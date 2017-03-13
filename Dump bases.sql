3ec07c63aee1f664a4425ba26a96a3847be26666
andreteixeiraUFS <dezaoteixeira@gmail.com>
2017-03-13 10:16:43 -0300

    Dump's: bases, línguas e países

---
+++ Dump bases.sql
@@ -0,0 +1,158 @@
+INSERT INTO public.search_engine(
+	id_search_engine, nm_search_engine)
+	VALUES (1,'MANUAL INSERT');
+	
+
+--Retirado de: https://en.wikipedia.org/wiki/List_of_academic_databases_and_search_engines --
+INSERT INTO search_engine(nm_search_engine) VALUES ('Academic Search');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Aerospace & High Technology Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('African Journals OnLine (AJOL)');
+INSERT INTO search_engine(nm_search_engine) VALUES ('AgeLine');
+INSERT INTO search_engine(nm_search_engine) VALUES ('AGRICOLA: Agricultural Online Access');
+INSERT INTO search_engine(nm_search_engine) VALUES ('AGRIS: Agricultural database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Airiti Inc');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Analytical Abstracts');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Analytical sciences digital library');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Anthropological Index Online');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Anthropological Literature');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Arachne');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Arnetminer');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Arts & Humanities Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('arXiv');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Association for Computing Machinery Digital Library');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Astrophysics Data System');
+INSERT INTO search_engine(nm_search_engine) VALUES ('ATLA Religion Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('AULIMP: Air University Library''s Index to Military Periodicals');
+INSERT INTO search_engine(nm_search_engine) VALUES ('BASE: Bielefeld Academic Search Engine');
+INSERT INTO search_engine(nm_search_engine) VALUES ('BDD: Diacronia Bibliometric Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Beilstein database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Biological Abstracts');
+INSERT INTO search_engine(nm_search_engine) VALUES ('BioOne');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Bioinformatic Harvester');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Book Review Index Online');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Books In Print');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CAB Abstracts');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Chemical Abstracts Service');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Chemisches Zentralblatt Structural Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('ChemXSeer');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Chinese Social Science Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Cochrane Library');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CINAHL: Cumulative Index to Nursing and Allied Health');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CiNii');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CHBD: Circumpolar Health Bibliographic Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Citebase Search');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CiteULike');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CiteSeer');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CiteSeerX');
+INSERT INTO search_engine(nm_search_engine) VALUES ('CogPrints: Cognitive Sciences Eprint Archives');
+INSERT INTO search_engine(nm_search_engine) VALUES ('The Collection of Computer Science Bibliographies');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Compendex');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Current Index to Statistics');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Current Contents');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Directory of Open Access Journals');
+INSERT INTO search_engine(nm_search_engine) VALUES ('dblp computer science bibliography');
+INSERT INTO search_engine(nm_search_engine) VALUES ('EconBiz');
+INSERT INTO search_engine(nm_search_engine) VALUES ('EconLit');
+INSERT INTO search_engine(nm_search_engine) VALUES ('EMBASE');
+INSERT INTO search_engine(nm_search_engine) VALUES ('ERIC: Educational Resource Information Center');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Europe PMC');
+INSERT INTO search_engine(nm_search_engine) VALUES ('FSTA – Food Science and Technology Abstracts');
+INSERT INTO search_engine(nm_search_engine) VALUES ('GENESIS');
+INSERT INTO search_engine(nm_search_engine) VALUES ('GeoRef');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Global Health');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Golm Metabolome Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Google Scholar');
+INSERT INTO search_engine(nm_search_engine) VALUES ('GoPubMed');
+INSERT INTO search_engine(nm_search_engine) VALUES ('HubMed');
+INSERT INTO search_engine(nm_search_engine) VALUES ('IEEE Xplore');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Index Copernicus');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Information Bridge: Department of Energy Sci. and Tech. Information');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Informit');
+INSERT INTO search_engine(nm_search_engine) VALUES ('IngentaConnect');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Indian Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('IARP');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Inspec');
+INSERT INTO search_engine(nm_search_engine) VALUES ('INSPIRE-HEP');
+INSERT INTO search_engine(nm_search_engine) VALUES ('International Directory of Philosophy');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Isidore');
+INSERT INTO search_engine(nm_search_engine) VALUES ('JournalSeek');
+INSERT INTO search_engine(nm_search_engine) VALUES ('JSTOR: Journal Storage');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Jurn');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Lesson Planet');
+INSERT INTO search_engine(nm_search_engine) VALUES ('LexisNexis');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Lingbuzz');
+INSERT INTO search_engine(nm_search_engine) VALUES ('MathSciNet');
+INSERT INTO search_engine(nm_search_engine) VALUES ('MedlinePlus');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Mendeley');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Merck Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Meteorological and Geoastrophysical Abstracts');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Microsoft Academic Search');
+INSERT INTO search_engine(nm_search_engine) VALUES ('NBER: National Bureau of Economic Research');
+INSERT INTO search_engine(nm_search_engine) VALUES ('National Criminal Justice Reference Service[98]');
+INSERT INTO search_engine(nm_search_engine) VALUES ('National Diet Library Collection');
+INSERT INTO search_engine(nm_search_engine) VALUES ('OAIster');
+INSERT INTO search_engine(nm_search_engine) VALUES ('OpenEdition');
+INSERT INTO search_engine(nm_search_engine) VALUES ('OpenSIGLE');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Philosophy Documentation Center eCollection');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Philosophy Research Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('PhilPapers');
+INSERT INTO search_engine(nm_search_engine) VALUES ('POIESIS: Philosophy Online Serials');
+INSERT INTO search_engine(nm_search_engine) VALUES ('POPLINE');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Project MUSE');
+INSERT INTO search_engine(nm_search_engine) VALUES ('PsycINFO');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Psychology''s Feminist Voices');
+INSERT INTO search_engine(nm_search_engine) VALUES ('PubChem');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Pubget');
+INSERT INTO search_engine(nm_search_engine) VALUES ('PubMed');
+INSERT INTO search_engine(nm_search_engine) VALUES ('PubPsych');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Questia: Online Research Library');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Readers'' Guide to Periodical Literature');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Reader''s Guide Retrospective: 1890–1982');
+INSERT INTO search_engine(nm_search_engine) VALUES ('RePEc: Research Papers in Economics');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Retina medical search');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Rock''s Backpages');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Russian Science Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SafetyLit');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SciELO');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Science.gov');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Science Accelerator');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Science Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('ScienceOpen');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Scientific Information Database(SID)');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Scopus');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SearchTeam');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Semantic Scholar');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Social Science Citation Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Socol@r: Socolar');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SPRESI Database');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SSRN: Social Science Research Network');
+INSERT INTO search_engine(nm_search_engine) VALUES ('INSPIRE-HEP');
+INSERT INTO search_engine(nm_search_engine) VALUES ('SpringerLink');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Ulrich''s Periodicals Directory');
+INSERT INTO search_engine(nm_search_engine) VALUES ('VET-Bib');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Web of Science');
+INSERT INTO search_engine(nm_search_engine) VALUES ('WestLaw');
+INSERT INTO search_engine(nm_search_engine) VALUES ('WorldCat');
+INSERT INTO search_engine(nm_search_engine) VALUES ('WorldWideScience');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Zasshi Kiji Sakuin: Japanese Periodicals Index');
+INSERT INTO search_engine(nm_search_engine) VALUES ('Zentralblatt MATH');
+INSERT INTO search_engine(nm_search_engine) VALUES ('The Zoological Record');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
