efa3acd8635b6b188e59bbd2acf8fa872bd7671b
glayderson <glayderson@mail.com>
2017-04-11 12:15:34 -0300

    Fixed comments on maintenance scripts

---
+++ Drop a specific user example.sql
@@ -0,0 +1,7 @@
+﻿REASSIGN OWNED BY nordsc_userfarmsp TO postgres
+DROP OWNED BY nordsc_userfarmsp
+DROP USER nordsc_userfarmsp
+
+REASSIGN OWNED BY nordsc_userfarmst TO postgres
+DROP OWNED BY nordsc_userfarmst
+DROP USER nordsc_userfarmst
